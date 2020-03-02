b6b93ce8479cf700ed4a0e032996490df45cea9d
Luis Pestana <lpestana@protonmail.com>
2020-03-02 02:47:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,7 +20,7 @@ def reduce_to_total(nums, starting_point = 0)
 end
 
 def reduce_to_all_true(array)
-  array.reduce { |word| print word}
+  array.reduce { |word| puts word }
 end
 
 # def reduce_to_any_true(array)
