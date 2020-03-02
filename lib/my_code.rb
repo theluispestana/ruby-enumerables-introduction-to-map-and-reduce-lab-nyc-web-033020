fac769a9dc5bde3b4e4ec61f4e966f46e435e86d
Luis Pestana <lpestana@protonmail.com>
2020-03-02 02:53:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,7 +21,7 @@ end
 
 def reduce_to_all_true(array)
   pp array
-  array.reduce { |memo, word| print "#{memo} #{word}" }
+  array.reduce { |memo, word| print "#{word}" }
 end
 
 # def reduce_to_any_true(array)
