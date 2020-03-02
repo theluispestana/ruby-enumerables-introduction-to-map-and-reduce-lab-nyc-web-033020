0c4f11e7c64b23a6e06af70f54db9c692789570b
Luis Pestana <lpestana@protonmail.com>
2020-03-02 01:51:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # My Code here....
+def map_to_negativize(array)
+  negative_array = array.map{ |num| num * -1 }
+end
+
+def map_to_no_change(arr)
+  same_arr = arr.map{ |x| x }
+end
+
+def map_to_double(nums)
+  doubled_nums = nums.map{ |num| num * 2 }
+end
+
+def map_to_square(nums)
+  squared_nums = nums.map{ |num| num ** 2 }
+end
+
+def reduce_to_total(nums, starting_point = nil)
+  if starting_point
+    nums.reduce(starting_point, :+)
+  else
+    nums.reduce(:+)
+  end
+  nums
+end
